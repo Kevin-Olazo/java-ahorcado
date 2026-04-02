e35331ffb7c1436ddcf5372bfb16141aac48d64b
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-04-02 16:01:47 -0500

    feat: agregar mensaje de bienvenida y inicializar el menú del juego Ahorcado

--- src/main/java/com/gadev/Main.java
+++ src/main/java/com/gadev/Main.java
@@ -2,7 +2,10 @@ package com.gadev;
 
 public class Main {
     public static void main(String[] args) {
+        MenuAhorcado menuAhorcado = new MenuAhorcado();
 
+        System.out.println("Bienvenido al juego del ahorcado!");
 
+        menuAhorcado.start();
     }
 }
